fe72bacce5997435da6bca6cd3d3f5d7aa4b934e
EvgeniyMedvedev <eposs3@mail.ru>
2023-09-25 23:22:20 +0300

    description

--- pkg/routes/route.go
+++ pkg/routes/route.go
@@ -15,6 +15,7 @@ func GetRoute() *gin.Engine {
 	r.POST("/login", handlers.Login)
 	r.POST("/registration", handlers.Registation)
 
+	//Проверка авторизации
 	r.Use(middleware.RequireAuth())
 
 	//Направления
@@ -22,6 +23,8 @@ func GetRoute() *gin.Engine {
 	r.GET("/direction/:id", handlers.GetDirection)
 	r.PATCH("/direction/", handlers.UpdateDirection)
 	r.DELETE("/direction/", handlers.DeleteDirection)
+
+	//TODO: Все остальные справочники
 
 	return r
 }
