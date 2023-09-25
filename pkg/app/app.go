8109c2099973b3b83c304e6e8d4aeaca213619c4
EvgeniyMedvedev <eposs3@mail.ru>
2023-09-25 23:57:13 +0300

    удалил таймзону

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,18 +2,14 @@ package app
 
 import (
 	"log"
-	"time"
 
 	"github.com/tegehhat/helper/pkg/database"
 	"github.com/tegehhat/helper/pkg/routes"
 )
 
 func Run() {
-	//Выставляем часовой пояс для приложения
-	_, err := time.LoadLocation("Europe/Moscow")
-
 	// Запускаем БД
-	err = database.OpenConnect()
+	err := database.OpenConnect()
 	if err != nil {
 		log.Fatal(err)
 	}
